0664fa4c11f59b59dc038ec667e0a02bee93dec7
Alyssa <alyssasgable@gmail.com>
2019-01-31 07:46:10 -0500

    added firebase pod

---
+++ AuburnHacks 2019/AuburnHacks 2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuburnHacks 2019
+//
+//  Created by Alyssa Gable on 1/30/19.
+//  Copyright © 2019 AuburnHacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AuburnHacks 2019/AuburnHacks 2019Tests/AuburnHacks_2019Tests.swift
@@ -0,0 +1,34 @@
+//
+//  AuburnHacks_2019Tests.swift
+//  AuburnHacks 2019Tests
+//
+//  Created by Alyssa Gable on 1/30/19.
+//  Copyright © 2019 AuburnHacks. All rights reserved.
+//
+
+import XCTest
+@testable import AuburnHacks_2019
+
+class AuburnHacks_2019Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
